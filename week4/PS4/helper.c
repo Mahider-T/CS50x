f025bb122e93af97f1999e2fd28633639a47699a
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-06-26 05:05:05 -0700

    Add functionality to teh grayscale function
    This works by changing RGB values of each pixel to the average of
    the original RGB values of the correspoding pixel

---
+++ week4/PS4/helper.c
@@ -0,0 +1,46 @@
+#include "helpers.h"
+#include <math.h>
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for ( int i = 0; i < height; i++){
+        for ( int j = 0; j < width; j++){
+            // int red;int green; int blue; int average;
+            // char *redc; char *greenc; char *bluec;
+
+            // red = strtol(image[i][j].rgbtRed, &redc ,16);
+            // green = strtol(image[i][j].rgbtGreen, &greenc ,16);
+            // blue = strtol(image[i][j].rgbtBlue, &bluec ,16);
+            // average = (red + blue + green)/3;
+
+            int average;
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+            average = round((red + green + blue)/3.0);
+
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
