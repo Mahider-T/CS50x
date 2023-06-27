5ab72de0697b3a474ca74626a1eb5a5662218a17
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-06-27 15:59:03 -0700

    Recovers all images correctly but does not pass valgrind test

--- week4/PS4/recover.c
+++ week4/PS4/recover.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    typedef uint8_t BYTE;
+     typedef uint8_t BYTE;
     BYTE block[512];
 
     if( argc != 2){
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
 
         int count = 0;
         int started = 0;
+        FILE *fileWrite;
     while (fread(block, 1, 512, file) == 512)
     {
         if(block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff /*&& (block[3] & 0xf0) == 0xe0*/){
@@ -31,12 +32,27 @@ int main(int argc, char *argv[])
             // count++;
             started = 1;
             count++;
-            printf("Start of an Image %i\n", count);
-            
+            char fileNow[9];
+            sprintf(fileNow, "%03i.jpg",count-1);
+            if(count != 1 && count != 0){
+                // char fileBefore[9];
+                // sprintf(fileBefore, "%03i.jpg",count-2);
+                fclose(fileWrite);
+            }
+            fileWrite = fopen(fileNow, "w");
+            fwrite(block, 1, 512, fileWrite);
+
+            printf("Start of an Image %i\n", count);//Debug
+
+
         }
         else{
             if(started == 1){
                 printf("Remaining of the image %i\n ", count);
+                 char fileNow[9];
+                 sprintf(fileNow, "%03i.jpg",count-1);
+                //  FILE *fileWrite = fopen(fileNow, "a");
+                 fwrite(block, 1, 512, fileWrite);
             }
         }
 
